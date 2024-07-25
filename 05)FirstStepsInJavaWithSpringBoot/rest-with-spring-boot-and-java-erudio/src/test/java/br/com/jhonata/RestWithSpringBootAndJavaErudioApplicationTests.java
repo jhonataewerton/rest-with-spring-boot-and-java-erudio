9fc8711e1e90f4e8ab64652d73fd90bf3f13fcbf
Jhonata Ewerton <jhonata.as@hotmail.com>
2024-07-25 19:46:14 -0300

    first steps in java with spring boot

---
+++ 05)FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/jhonata/RestWithSpringBootAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhonata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
